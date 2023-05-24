b494f895337015eb8184fb2b48117a4167e31ce7
bradpreston <bap5393@gmail.com>
2023-05-23 20:05:37 -0400

    test redirect again

--- pages/api/verifyPassword.ts
+++ pages/api/verifyPassword.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { redirect } from "next/navigation"
 import bcrypt from "bcrypt"
 
 export default async function verifyPassword(
@@ -9,7 +10,7 @@ export default async function verifyPassword(
 		const hash = await bcrypt.hash(req.body.password, 10)
 		const acceptedPassword: string = process.env.EPK_ACCESS_PASSWORD!
 		const valid = await bcrypt.compare(acceptedPassword, hash)
-		if (valid) res.redirect("/test")
+		if (valid) res.status(200).json("ok")
 		else res.status(401).json("not authorized")
 	} catch (err) {
 		res.status(400).json(`an error ocurred in get password hash: ${err}`)
