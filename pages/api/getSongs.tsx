5dc79391cd40091f4dd3e34e60fe889ee507b22f
bradpreston <bap5393@gmail.com>
2023-05-25 16:29:24 -0400

    add cors package and middleware

--- pages/api/getSongs.tsx
+++ pages/api/getSongs.tsx
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { S3, GetObjectCommand } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import Cors from "cors"
 
 type song = {
 	title: string
@@ -9,10 +10,36 @@ type song = {
 	length: number
 }
 
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+	methods: ["POST", "GET", "HEAD"],
+	origin: "*"
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+	req: NextApiRequest,
+	res: NextApiResponse,
+	fn: Function
+) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result: any) => {
+			if (result instanceof Error) {
+				return reject(result)
+			}
+
+			return resolve(result)
+		})
+	})
+}
+
 export default async function GetSongs(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
+	await runMiddleware(req, res, cors)
 	const body = JSON.parse(req.body)
 
 	try {
