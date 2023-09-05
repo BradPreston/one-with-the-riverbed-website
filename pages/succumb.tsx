f77ffa837d6db4b26c39c8dd5cd4707cdacc3b7e
bradpreston <bap5393@gmail.com>
2023-09-05 18:38:38 -0400

    Add image with logo to EPK

--- pages/succumb.tsx
+++ pages/succumb.tsx
@@ -76,7 +76,49 @@ export default function EPK({ data }: Props) {
 
 	return (
 		<>
+		<div className="relative w-full max-w-3xl aspect-4/3 m-auto">
+				<Image src="/images/image-with-logo.png" alt="One with the riverbed promo picture" fill className="object-contain" />
+			</div>   
+
 			<div id="bio" className="max-w-3xl flex flex-col m-auto mb-5 text-sand">
+			<p className="mb-5">
+					One with the Riverbed is a five-piece post-metal band from Kalamazoo,
+					Michigan. Since forming in 2017, the band has strived to push the
+					boundaries of the genre by incorporating elements of atmospheric black
+					metal with dissonant textures and dynamic shifts. Absence, the band’s
+					first full-length album following an EP in 2018, garnered
+					international attention upon its release in 2021, earning accolades
+					from publications such as Metal Hammer, No Clean Singing, and Toilet
+					ov Hell.{" "}
+				</p>
+
+				{/* {showBio ? (
+					<> */}
+						<p className="mb-5">
+							In preparation of releasing their second full-length album,
+							Succumb, One with the Riverbed seeks collaboration with a label
+							that can provide distribution, promotion, and artist development.
+							With a distinct fusion of haunting melodies, visceral riffs, and
+							enveloping soundscapes, Succumb shows the band embracing their
+							core musical identity while venturing into previously unexplored
+							realms. Lyrical themes include nostalgia, temporality, and the
+							human condition.
+						</p>
+
+						<p className="mb-5">
+							Thank you for your time and consideration. Select tracks from
+							Succumb are available to stream below.
+						</p>
+					{/* </>
+				) : null} */}
+
+				{/* <button
+					className="mx-auto flex mb-5 text-sand underline"
+					onClick={showOrHideBio}
+				>
+					Show {showBio ? "Less" : "More"}
+				</button> */}
+
 				<div
 					className={`flex ${
 						isMobile ? "flex-col" : null
@@ -92,43 +134,7 @@ export default function EPK({ data }: Props) {
 					{data && <AudioPlayer playlist={tracklist}></AudioPlayer>}
 				</div>
 
-				<p className="mb-5">
-					One with the Riverbed is a five-piece post-metal band from Kalamazoo,
-					Michigan. Since forming in 2017, the band has strived to push the
-					boundaries of the genre by incorporating elements of atmospheric black
-					metal with dissonant textures and dynamic shifts. Absence, the band’s
-					first full-length album following an EP in 2018, garnered
-					international attention upon its release in 2021, earning accolades
-					from publications such as Metal Hammer, No Clean Singing, and Toilet
-					ov Hell.{" "}
-				</p>
-
-				{showBio ? (
-					<>
-						<p className="mb-5">
-							In preparation of releasing their second full-length album,
-							Succumb, One with the Riverbed seeks collaboration with a label
-							that can provide distribution, promotion, and artist development.
-							With a distinct fusion of haunting melodies, visceral riffs, and
-							enveloping soundscapes, Succumb shows the band embracing their
-							core musical identity while venturing into previously unexplored
-							realms. Lyrical themes include nostalgia, temporality, and the
-							human condition.
-						</p>
-
-						<p>
-							Thank you for your time and consideration. Select tracks from
-							Succumb are available to stream below.
-						</p>
-					</>
-				) : null}
-
-				<button
-					className="mx-auto flex mb-5 text-sand underline"
-					onClick={showOrHideBio}
-				>
-					Show {showBio ? "Less" : "More"}
-				</button>
+				
 			</div>
 		</>
 	)
