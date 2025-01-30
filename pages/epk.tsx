3d8041d79ccefc3d1827f0ceb20d1a32a63729d4
brad preston <bap5393@gmail.com>
2025-01-30 18:40:46 -0500

    Update EPK bio

--- pages/epk.tsx
+++ pages/epk.tsx
@@ -14,29 +14,51 @@ export default function EPK() {
 
 			<div id="bio" className="max-w-3xl flex flex-col m-auto mb-5 text-sand">
 				<p className="mb-5">
-					One with the Riverbed is a five-piece post-metal band from Kalamazoo,
-					Michigan. Since forming in 2017, the band has strived to push the
-					boundaries of the genre by incorporating elements of atmospheric black
-					metal with dissonant textures and dynamic shifts. Absence, the band’s
-					first full-length album following an EP in 2018, garnered
-					international attention upon its release in 2021, earning accolades
-					from publications such as Metal Hammer, No Clean Singing, and Toilet
-					ov Hell.{" "}
+					<strong>One with the Riverbed</strong> is a five-piece post-metal band
+					hailing from Kalamazoo, Michigan. Since their formation in 2017, the
+					band has carved out a unique space in the genre by blending
+					atmospheric black metal with dissonant textures, dynamic shifts, and
+					emotionally charged soundscapes.
 				</p>
 
 				<p className="mb-5">
-					In preparation of releasing their second full-length album, Succumb,
-					One with the Riverbed seeks collaboration with a label that can
-					provide distribution, promotion, and artist development. With a
-					distinct fusion of haunting melodies, visceral riffs, and enveloping
-					soundscapes, Succumb shows the band embracing their core musical
-					identity while venturing into previously unexplored realms. Lyrical
-					themes include nostalgia, temporality, and the human condition.
+					Their debut full-length album, Absence (2021), marked a significant
+					milestone for the band, earning international acclaim from
+					publications like Metal Hammer, No Clean Singing, and Toilet ov Hell.
+					Building on the momentum of their 2018 EP, Absence showcased the
+					band's ability to craft immersive, genre-defying music that resonates
+					deeply with listeners.
 				</p>
 
 				<p className="mb-5">
-					Thank you for your time and consideration. Select tracks from Succumb
-					are available to stream below.
+					With their sophomore album, Succumb, One with the Riverbed has further
+					refined their sound, embracing their core musical identity while
+					venturing into uncharted creative territories. The album has received
+					high praise from renowned publications such as Decibel Magazine and
+					Metal Temple, with critics applauding its depth and innovation.
+					Additionally, Succumb has been recognized by prominent Spotify
+					playlist curators, including Season of Mist and Arctic Drones.
+					Exploring themes of nostalgia, temporality, and the human condition,
+					the album delivers a powerful and introspective listening experience.
+				</p>
+
+				<p className="mb-5">
+					One with the Riverbed has prioritized building their presence through
+					consistent touring and live performances. As they continue to expand
+					their reach, One with the Riverbed is eager to bring their powerful
+					performances to new regions and audiences.
+				</p>
+
+				<p className="mb-5">
+					For press inquiries, booking, or collaboration opportunities, please
+					reach out to{" "}
+					<a
+						href="mailto:onewiththeriverbed@gmail.com"
+						className="border-b-[1px] border-b-sand hover:border-b-[0px] transition-all"
+					>
+						onewiththeriverbed@gmail.com
+					</a>
+					.
 				</p>
 
 				<iframe
